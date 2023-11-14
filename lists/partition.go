7cff582beb80e7a469c9f85d6bdfbf9e9a8ceb80
Reiner Hüchting <dev@reinerhuechting.de>
2023-11-14 19:15:04 +0100

    Remove hints.

--- lists/partition.go
+++ lists/partition.go
@@ -4,10 +4,6 @@ package lists
 // - Eine, die alle Elemente aus list enthält, die kleiner oder gleich key sind.
 // - Eine, die alle übrigen Elemente aus list enthält.
 func Partition(list []int, key int) ([]int, []int) {
-	// Verwende Kopien von list, damit die ursprüngliche Liste nicht verändert wird.
-	l1 := append([]int{}, list...)
-	l2 := append([]int{}, list...)
-
 	// TODO
 	return l1, l2
 }
