3e61688021dc52179e54ec66d07fa4e2e464e606
Reiner Hüchting <dev@reinerhuechting.de>
2023-11-15 13:28:06 +0100

    Fix broken task.

--- lists/partition.go
+++ lists/partition.go
@@ -4,6 +4,8 @@ package lists
 // - Eine, die alle Elemente aus list enthält, die kleiner oder gleich key sind.
 // - Eine, die alle übrigen Elemente aus list enthält.
 func Partition(list []int, key int) ([]int, []int) {
+	l1 := make([]int, 0)
+	l2 := make([]int, 0)
 	// TODO
 	return l1, l2
 }
